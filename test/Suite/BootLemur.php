b376ce76b07da3aa3da4a87ffafce3f42050f1f4
Pierre-Philippe Emond <emondpph@gmail.com>
2022-05-23 11:08:31 -0400

    boot lemur bug

--- test/Suite/BootLemur.php
+++ test/Suite/BootLemur.php
@@ -35,7 +35,7 @@ class BootLemur extends Test\Suite\BootCore
             Lemur\Col::class=>[
                 'generalExcerpt'=>100]],
         'service'=>[
-            'browser'=>[Lemur\Service\Navigation::class,['test'=>true]],
+            'navigation'=>[Lemur\Service\Navigation::class,['test'=>true]],
             'tinymce'=>null,
             'sortable'=>null],
         'compile'=>true,
